0fcb65e115990e8d95bbbbc457eff26546a6f8dd
jmavandi <jonathan.mavandi@gmail.com>
2020-06-22 23:33:40 -0700

    setup route for inventory

---
+++ ebay-dash/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import toppsLogo from './topps2020.jpeg';
+import inventoryPic from './inventory.jpeg'
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, } from "react-router-dom";
+import { Image, Menu, Button } from 'semantic-ui-react';
+
+import Inventory from './components/inventory.js';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="Ebay-Dash">
+          <img src={toppsLogo} className="Topps-Logo" alt="topp-logo" />
+        </header>
+        <Menu>
+          <Menu.Item>
+          <Link to='/inventory'> <Button >  <Image src={inventoryPic} /> </Button> </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Button>Log-in</Button>
+          </Menu.Item>
+        </Menu>
+
+        <Switch>
+          <Route path="/inventory">
+            <Inventory/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ ebay-dash/src/components/inventory.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+          <div>
+            Heres the inventory
+          </div>
+      );
+    }
+}
+
+export default Inventory;
